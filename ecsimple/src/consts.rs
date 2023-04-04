0bef7714d532f7f35777562c2425c2d4fc2715cc
jeppeter <jeppeter@gmail.com>
2023-04-04 16:37:52 +0800

    to add SECT113r1

--- ecsimple/src/consts.rs
+++ ecsimple/src/consts.rs
@@ -2,6 +2,7 @@
 pub const SECT163k1_NAME :&str = "SECT163k1";
 pub const SECT163r1_NAME :&str = "SECT163r1";
 pub const SECT239k1_NAME :&str = "SECT239k1";
+pub const SECT113r1_NAME :&str = "SECT113r1";
 
 pub const SECP112r1_NAME :&str = "SECP112r1";
 pub const SECP112r2_NAME :&str = "SECP112r2";
@@ -10,6 +11,7 @@ pub const SECP112r2_NAME :&str = "SECP112r2";
 pub const SECT163k1_OID :&str = "1.3.132.0.1";
 pub const SECT163r1_OID :&str = "1.3.132.0.2";
 pub const SECT239k1_OID :&str = "1.3.132.0.3";
+pub const SECT113r1_OID :&str = "1.3.132.0.4";
 pub const SECP112r1_OID :&str = "1.3.132.0.6";
 pub const SECP112r2_OID :&str = "1.3.132.0.7";
 
