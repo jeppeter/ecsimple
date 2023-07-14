449221c05c92bc1eac1670e4f30c7129edfbdb46
jeppeter <jeppeter@gmail.com>
2023-07-14 11:00:33 +0800

    to make cargo build

--- ecsimple/utest/ectst/runsign.bat
+++ ecsimple/utest/ectst/runsign.bat
@@ -5,5 +5,4 @@ set OUTFILE=z:\rust.log
 set SIMPLEOUT=z:\rust_simple.log
 set ECSIMPLE_LEVEL=50
 set ECSIMPLE_RANDOP=z:\rand.bin
-%CURDIR%\target\release\ectst.exe ecsignbase SECT163k1 1152 1152 2> %OUTFILE%
-python z:\pylib\utils.py -i %OUTFILE% -o %SIMPLEOUT% filterlog rust
+pushd %CD% && cd %CURDIR% && cargo build --release && .\target\release\ectst.exe ecsignbase SECT163k1 1152 1152 2> %OUTFILE% && python z:\pylib\utils.py -i %OUTFILE% -o %SIMPLEOUT% filterlog rust && popd
