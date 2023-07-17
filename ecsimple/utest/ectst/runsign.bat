e1344cf307d4970565a7f5d7a46cb1332d348e50
jeppeter <jeppeter@gmail.com>
2023-07-17 16:26:06 +0800

    to make runsign.bat

--- ecsimple/utest/ectst/runsign.bat
+++ ecsimple/utest/ectst/runsign.bat
@@ -5,4 +5,6 @@ set OUTFILE=z:\rust.log
 set SIMPLEOUT=z:\rust_simple.log
 set ECSIMPLE_LEVEL=50
 set ECSIMPLE_RANDOP=z:\rand.bin
-pushd %CD% && cd %CURDIR% && cargo build --release && .\target\release\ectst.exe ecsignbase SECT163k1 1152 1152 2> %OUTFILE% && python z:\pylib\utils.py -i %OUTFILE% -o %SIMPLEOUT% filterlog rust && popd
+set PRIVNUMB=1152
+set HASHNUM=7201
+pushd %CD% && cd %CURDIR% && cargo build --release && .\target\release\ectst.exe ecsignbase SECT163k1 %PRIVNUMB% %HASHNUM% 2> %OUTFILE% && python z:\pylib\utils.py -i %OUTFILE% -o %SIMPLEOUT% filterlog rust && popd
