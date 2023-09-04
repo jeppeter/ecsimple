c1ee511643e83017b5fde5526037f2bfa44fb6f7
jeppeter <jeppeter@gmail.com>
2023-09-04 13:49:58 +0800

    to pass SECP224k1

--- ecsimple/src/group.rs
+++ ecsimple/src/group.rs
@@ -1073,6 +1073,45 @@ fn create_group_prime_curves() -> HashMap<String,ECGroupPrime> {
 	bngrp.specflags = 0;
 	retv.insert(SECP192k1_NAME.to_string(),bngrp.clone());
 
+	/*secp224k1*/
+	v8 = Vec::from_hex("fffffffffffffffffffffffffffffffffffffffffffffffeffffe56d").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	bngrp.p = p.clone();
+	montv = MontNum::new(&bngrp.p).unwrap();
+	tmpp = p.clone();
+	v8 = Vec::from_hex("00000000000000000000000000000000000000000000000000000000").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	tmpa = p.clone();
+	bngrp.a = montv.mont_to(&p);
+	v8 = Vec::from_hex("00000000000000000000000000000000000000000000000000000005").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	bngrp.b = montv.mont_to(&p);
+	v8 = Vec::from_hex("a1455b334df099df30fc28a169a467e9e47075a90f7e650eb6b7a45c").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	bngrp.generator.x = montv.mont_to(&p);
+	v8 = Vec::from_hex("7e089fed7fba344282cafbd6f7e319f7c0b0bd59e2ca4bdb556d61a5").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	bngrp.generator.y = montv.mont_to(&p);
+	bngrp.generator.z = montv.mont_to(&ov);
+
+	v8 = Vec::from_hex("010000000000000000000000000001dce8d2ec6184caf0a971769fb1f7").unwrap();
+	p = BigInt::from_bytes_be(Sign::Plus,&v8);
+	bngrp.order = p.clone();
+	bngrp.cofactor = ov.clone();
+	bngrp.curvename = SECP224k1_NAME.to_string();
+
+	//ecsimple_log_trace!("tmpp 0x{:X} tmpa 0x{:X}",tmpp,tmpa);
+	if tmpp == (tmpa.clone() + ov.clone() + ov.clone() + ov.clone()) {
+		bngrp.is_minus3 = true;
+		//ecsimple_log_trace!("{} is_minus3 true",SECP224k1_NAME);
+	} else {
+		bngrp.is_minus3 = false;
+		//ecsimple_log_trace!("{} is_minus3 false",SECP224k1_NAME);
+	}
+	bngrp.specflags = 0;
+	retv.insert(SECP224k1_NAME.to_string(),bngrp.clone());
+
+
 	retv
 }
 
