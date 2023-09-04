2549fd826b4f0a78f818e622c6d70b217a8b9474
jeppeter <jeppeter@gmail.com>
2023-09-04 09:58:42 +0800

    to make extractsign and encapsign ok

--- ecsimple/utest/ectst/src/ectest.rs
+++ ecsimple/utest/ectst/src/ectest.rs
@@ -25,10 +25,11 @@ use super::loglib::*;
 #[allow(unused_imports)]
 use super::fileop::*;
 use super::strop::*;
+use super::*;
 #[allow(unused_imports)]
 use std::io::Write;
 
-use num_bigint::{BigInt};
+use num_bigint::{BigInt,Sign};
 
 use ecsimple::group::{ECGroupPrime,get_prime_group_curve};
 use ecsimple::point::ECPrimePoint;
@@ -140,7 +141,68 @@ fn ecpubload_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetIm
 
 	Ok(())
 }
-#[extargs_map_function(ecgen_handler,ecsignbase_handler,ecvfybase_handler,ecpubload_handler)]
+
+
+fn extractsign_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {
+
+	init_log(ns.clone())?;
+	let input :String = ns.get_string("input");
+	let output :String = ns.get_string("output");
+
+	let sigdata :Vec<u8> = read_file_bytes(&input)?;
+	let sig :ECSignature = ECSignature::decode_asn1(&sigdata)?;
+	let mut outdata :Vec<u8> = Vec::new();
+	let (_,r) = sig.r.to_bytes_be();
+	let (_,s) = sig.s.to_bytes_be();
+	outdata.extend(r);
+	outdata.extend(s);
+	if output.len() > 0 {
+		write_file_bytes(&output,&outdata)?;
+	} else {
+		debug_buffer_trace!(outdata.as_ptr(),outdata.len(),"outdata ");
+	}
+
+
+	Ok(())
+}
+
+fn encapsign_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {
+
+	init_log(ns.clone())?;
+	let input :String = ns.get_string("input");
+	let output :String = ns.get_string("output");
+
+	let sigdata :Vec<u8> = read_file_bytes(&input)?;
+	let siglen :usize = sigdata.len() >> 1;
+	let mut r :Vec<u8> = Vec::new();
+	let mut idx :usize;
+	idx = 0;
+	while idx < siglen {
+		r.push(sigdata[idx]);
+		idx += 1;
+	}
+	let mut s :Vec<u8> = Vec::new();
+	idx = siglen;
+	while idx < sigdata.len() {
+		s.push(sigdata[idx]);
+		idx += 1;
+	}
+	let rbn :BigInt = BigInt::from_bytes_be(Sign::Plus,&r);
+	let sbn :BigInt = BigInt::from_bytes_be(Sign::Plus,&s);
+	let sig :ECSignature = ECSignature::new(&rbn,&sbn);
+	let data :Vec<u8> = sig.encode_asn1()?;
+	if output.len() > 0 {
+		write_file_bytes(&output,&data)?;
+	} else {
+		debug_buffer_trace!(data.as_ptr(),data.len(),"outdata ");
+	}
+
+
+	Ok(())
+}
+
+
+#[extargs_map_function(ecgen_handler,ecsignbase_handler,ecvfybase_handler,ecpubload_handler,extractsign_handler,encapsign_handler)]
 pub fn ec_load_parser(parser :ExtArgsParser) -> Result<(),Box<dyn Error>> {
 	let cmdline = r#"
 	{
@@ -155,6 +217,12 @@ pub fn ec_load_parser(parser :ExtArgsParser) -> Result<(),Box<dyn Error>> {
 		},
 		"ecpubload<ecpubload_handler>##ecname pubbin to load ec public key##" : {
 			"$" : 2
+		},
+		"extractsign<extractsign_handler>##input input and output for output##" : {
+			"$" : 0
+		},
+		"encapsign<encapsign_handler>##input input and output for output for input sign##" : {
+			"$" : 0
 		}
 	}
 	"#;
