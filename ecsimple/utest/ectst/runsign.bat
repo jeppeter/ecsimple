de4dcc60de8be0af98a6414b41399b8f92bbffe1
jeppeter <jeppeter@gmail.com>
2023-09-04 10:46:34 +0800

    to make SECP224r1 ok with openssl

--- ecsimple/utest/ectst/runsign.bat
+++ ecsimple/utest/ectst/runsign.bat
@@ -6,6 +6,7 @@ set SIMPLEOUT=z:\rust_simple.log
 set ECSIMPLE_LEVEL=50
 set ECSIMPLE_RANDOP=z:\rand.bin
 set PRIVNUMB=1152
-set ECTYPE=SECP112r1
+REM set ECTYPE=SECP112r1
+set ECTYPE=SECP224r1
 set HASHNUM=7201
 pushd %CD% && cd %CURDIR% && cargo build --release && .\target\release\ectst.exe ecsignbase %ECTYPE% %PRIVNUMB% %HASHNUM% 2> %OUTFILE% && python z:\pylib\utils.py -i %OUTFILE% -o %SIMPLEOUT% filterlog rust && popd
