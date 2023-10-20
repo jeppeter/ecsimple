1037d68a0e408224a14729d6cabbf0b815a0f99b
jeppeter <jeppeter@gmail.com>
2023-10-20 09:43:43 +0800

    to make example into doc

--- ecsimple/README.md
+++ ecsimple/README.md
@@ -6,4 +6,41 @@
 
 ### simple example
 ```rust
+use ecsimple::keys::*;
+use ecsimple::group::*;
+use ecsimple::signature::*;
+
+use std::error::Error;
+
+fn main() -> Result<(),Box<dyn Error>> {
+	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
+    let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
+    let pubkey :ECPublicKey = privkey.export_pubkey();
+    let signhash :Vec<u8> = vec![10,11,12,13,14];
+    let sig :ECSignature = privkey.sign_base(&signhash)?;
+    let retval :bool = pubkey.verify_base(&sig,&signhash)?;
+    assert!(retval == true);
+    Ok(())
+}
+
+```
+
+### from and to der
+```rust
+use ecsimple::keys::*;
+use ecsimple::group::*;
+
+use std::error::Error;
+
+fn main() -> Result<(),Box<dyn Error>> {
+	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
+    let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
+    let pubkey :ECPublicKey = privkey.export_pubkey();
+    let privdata :Vec<u8> = privkey.to_der("compressed","")?;
+    let pubdata  :Vec<u8> = pubkey.to_der("compressed","explicit")?;
+    let _nprivkey :ECPrivateKey = ECPrivateKey::from_der(&privdata)?;
+    let _npubkey :ECPublicKey = ECPublicKey::from_der(&pubdata)?;
+    Ok(())
+}
+
 ```
--- ecsimple/src/lib.rs
+++ ecsimple/src/lib.rs
@@ -1,3 +1,43 @@
+//!    simple implementation for Elliptic Curve Cryptography
+//!    to sign and verify
+//!    ```rust
+//!    use ecsimple::keys::*;
+//!    use ecsimple::group::*;
+//!    use ecsimple::signature::*;
+//!    
+//!    use std::error::Error;
+//!    
+//!    fn main() -> Result<(),Box<dyn Error>> {
+//!    	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
+//!        let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
+//!        let pubkey :ECPublicKey = privkey.export_pubkey();
+//!        let signhash :Vec<u8> = vec![10,11,12,13,14];
+//!        let sig :ECSignature = privkey.sign_base(&signhash)?;
+//!        let retval :bool = pubkey.verify_base(&sig,&signhash)?;
+//!        assert!(retval == true);
+//!        Ok(())
+//!    }
+//!    
+//!    ```
+//!    to use from_der and to_der
+//!    ```rust
+//!    use ecsimple::keys::*;
+//!    use ecsimple::group::*;
+//!    
+//!    use std::error::Error;
+//!    
+//!    fn main() -> Result<(),Box<dyn Error>> {
+//!    	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
+//!        let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
+//!        let pubkey :ECPublicKey = privkey.export_pubkey();
+//!        let privdata :Vec<u8> = privkey.to_der("compressed","")?;
+//!        let pubdata  :Vec<u8> = pubkey.to_der("compressed","explicit")?;
+//!        let _nprivkey :ECPrivateKey = ECPrivateKey::from_der(&privdata)?;
+//!        let _npubkey :ECPublicKey = ECPublicKey::from_der(&pubdata)?;
+//!        Ok(())
+//!    }
+//!    
+//!    ```
 
 #[allow(dead_code)]
 #[allow(non_upper_case_globals)]
