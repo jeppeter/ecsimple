92d4d5a388ec4f3d00f5a386d2ad071d013e6b2e
jeppeter <jeppeter@gmail.com>
2023-10-23 09:39:39 +0800

    to make examples with clear data

--- src/lib.rs
+++ src/lib.rs
@@ -7,33 +7,36 @@
 //!    
 //!    use std::error::Error;
 //!    
-//!    fn main() -> Result<(),Box<dyn Error>> {
-//!    	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
-//!        let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
-//!        let pubkey :ECPublicKey = privkey.export_pubkey();
-//!        let signhash :Vec<u8> = vec![10,11,12,13,14];
-//!        let sig :ECSignature = privkey.sign_base(&signhash)?;
-//!        let retval :bool = pubkey.verify_base(&sig,&signhash)?;
-//!        assert!(retval == true);
-//!        Ok(())
+//!    use sha1::{Sha1,Digest};
+//!    
+//!    fn get_hash_value(data :&[u8]) -> Vec<u8> {
+//!    	let mut hasher = Sha1::new();
+//!    	hasher.update(data);
+//!    	return hasher.finalize().to_vec();
 //!    }
 //!    
-//!    ```
-//!    to use from_der and to_der
-//!    ```rust
-//!    use ecsimple::keys::*;
-//!    use ecsimple::group::*;
-//!    
-//!    use std::error::Error;
-//!    
+//!    /*
+//!    	this is command like
+//!    	openssl ecparam -genkey -name prime256v1 -noout -out ecpriv.pem
+//!    	openssl ec -in ecpriv.pem -pubout -out ecpub.pem
+//!    	openssl dgst -sha1 -sign ecpriv.pem -out sig.bin realdata.bin
+//!    	openssl dgst -sha1 -verify ecpub.pem realdata.bin
+//!    */
 //!    fn main() -> Result<(),Box<dyn Error>> {
 //!    	let grp :ECGroup = ecc_get_curve_group("prime256v1")?;
 //!        let privkey :ECPrivateKey = ECPrivateKey::generate(&grp);
 //!        let pubkey :ECPublicKey = privkey.export_pubkey();
-//!        let privdata :Vec<u8> = privkey.to_der("compressed","")?;
-//!        let pubdata  :Vec<u8> = pubkey.to_der("compressed","explicit")?;
-//!        let _nprivkey :ECPrivateKey = ECPrivateKey::from_der(&privdata)?;
-//!        let _npubkey :ECPublicKey = ECPublicKey::from_der(&pubdata)?;
+//!        let realdata :Vec<u8> = vec![0x11,0x22,0x33];
+//!        let signhash :Vec<u8> = get_hash_value(&realdata);
+//!        let sig :ECSignature = privkey.sign_base(&signhash)?;
+//!        let retval :bool = pubkey.verify_base(&sig,&signhash)?;
+//!        /*
+//!        	privdata = privkey.to_der("compressed","")?;
+//!        	pubdata = pubkey.to_der("compressed","")?;
+//!        	sigdata = sig.encode_asn1()?;
+//!    
+//!        */
+//!        assert!(retval == true);
 //!        Ok(())
 //!    }
 //!    
